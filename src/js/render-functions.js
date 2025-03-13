aac6e572ae7bfbdb628c3c958c297c05213512d7
Mykhajlo <bmve@ukr.net>
2025-03-13 17:47:29 +0200

    fix scroll load

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -2,37 +2,33 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { refs } from './refs';
 
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionData: 'alt',
+    captionDelay: 250,
+  });
+
+  
 export const renderImages = images => {
   const gallery = refs.gallery;
   const galleryItems = images
     .map(
       image =>
         ` <li class="gallery-item">
-          <a class="gallery-link" href="${image.webformatURL}"> <img class="gallery-img" src="${image.largeImageURL}" alt="${image.tags}" /></a>
-          <table>
-           <table>
-            <tr>
-              <th>Likes</th> 
-              <th>Views</th>
-              <th>Comments</th>
-              <th>Downloads</th>
-            </tr>
-            <tr>
-             <td>${image.likes}</td>
-             <td>${image.views}</td>
-             <td>${image.comments}</td>
-             <td>${image.downloads}</td>
-            </tr>
-          </table>
-        </li>`
+  <a class="gallery-link" href="${image.largeImageURL}">
+    <img class="gallery-img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+    <div class="image-info">
+      <p><strong>Likes</strong> ${image.likes}</p>
+      <p><strong>Views</strong> ${image.views}</p>
+      <p><strong>Comments</strong> ${image.comments}</p>
+      <p><strong>Downloads</strong> ${image.downloads}</p>
+    </div>
+  </a>
+</li>`
     )
     .join('');
 
   gallery.insertAdjacentHTML('beforeend', galleryItems);
 
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionData: 'alt',
-    captionDelay: 250,
-  });
+
   lightbox.refresh();
 };
--- src/main.js
+++ src/main.js
@@ -5,39 +5,51 @@ import { refs } from './js/refs';
 import { renderImages } from './js/render-functions';
 import iconError from './img/error.svg';
 import { showLoader, hideLoader } from './js/loader';
-import { Input } from 'postcss';
 
 let page = 1;
 let isLoading = false;
+let currentQuery = '';
 
-refs.inputQuery.addEventListener('input', () => {
-  if (refs.inputQuery.value.trim() === '') {
-    refs.btnSearch.setAttribute('disabled', true);
-  }
+refs.form.addEventListener('input', () => {
+    const queryInput = refs.inputQuery.value.trim();
+  if (queryInput === '') {
+      refs.btnSearch.setAttribute('disabled', true);
+      refs.inputQuery.value = '';
+      iziToast.warning({
+          title: 'Caution',
+          message:
+            'Sorry, the input cannot be empty or contain only spaces!',
+           position: 'bottomLeft',
+          maxWidth: 300,
+          timeout: 1000,
+        });
+  } else {
   refs.btnSearch.removeAttribute('disabled');
+  }
 });
 
 refs.form.addEventListener('submit', event => {
   event.preventDefault();
-  const query = refs.inputQuery.value.trim();
+    const query = refs.inputQuery.value.trim();
+    currentQuery = query;
   if (query) {
     page = 1;
     refs.gallery.innerHTML = '';
-    loadImages(query);
+    loadImages();
   }
   refs.inputQuery.value = '';
 });
 
-const loadImages = query => {
+const loadImages = () => {
   if (isLoading) {
     return;
   }
   isLoading = true;
   showLoader();
-  fetchImages(query, page)
+  fetchImages(currentQuery, page)
     .then(images => {
       if (images.length === 0) {
-        iziToast.show({
+         iziToast.error({
           message:
             ' Sorry, there are no images matching your search query.Please try again!',
           messageColor: '#FAFAFB',
@@ -45,9 +57,9 @@ const loadImages = query => {
           backgroundColor: '#EF4040',
           borderColor: '#FFBEBE',
           maxWidth: 432,
-          theme: 'dark',
           class: 'custom-toast',
           position: 'topRight',
+          theme: 'dark',
           iconUrl: iconError,
         });
       } else {
@@ -59,15 +71,15 @@ const loadImages = query => {
       console.error('Error:', error);
     })
     .finally(() => {
-      isLoading = false;
-      hideLoader();
+        isLoading = false;
+        hideLoader();     
     });
 };
-const loadScroll = query => {
+const loadScroll = () => {
   const scrollPosition = window.scrollY + window.innerHeight;
   const pageHeight = document.documentElement.scrollHeight;
   if (scrollPosition >= pageHeight - 200) {
-    loadImages(query);
+    loadImages();
   }
 };
 window.addEventListener('scroll', loadScroll);
